bc280bf1704186476cb1113de0ce092c4ac973b0
mseidler <pain500@hotmail.de>
2017-12-05 16:06:48 +0100

    cleanup

--- src/de/init/javamailer/util/HtmlGenerator.java
+++ src/de/init/javamailer/util/HtmlGenerator.java
@@ -10,15 +10,6 @@ public class HtmlGenerator {
 	private final String cid = "radio_image";
 	private final String subject = "]init[RADIO kommt!";
 
-	// private String generateHTMLBodyContent() {
-	// return "<html><head>" + "<title>This is not usually displayed</title>" +
-	// "</head>\n"
-	// + "<body><div><strong>Hi there!</strong></div>"
-	// + "<div>Sending HTML in email is so <em>cool!</em> </div>\n"
-	// + "<div>And here's an image: <img src=\"cid:" + cid + "\" /></div>n"
-	// + "<div>I hope you like it!</div></body></html>";
-	// }
-
 	private String generateHTMLBodyContent() {
 		return "<html><head>" + "<title>Radio</title>" + "</head>\n"
 				+ "<body><div><strong>&hellip; fast ist es soweit!</strong></div><br>"
